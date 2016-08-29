7c8c8e0c7b50cd22ca0613f54d141c0db6e89a32
mars0i <mabrams@uab.edu>
2016-08-28 23:49:58 -0500

    added comment

--- project.clj
+++ project.clj
@@ -9,5 +9,5 @@
                  [thinktopic/aljabr "0.1.1"] ; ignore [... "0.4.0-SNAPSHOT"] in README.md--wishful thinking
                  [clatrix "0.5.0"]
                  ;[org.clojars.ds923y/nd4clj "0.1.0-SNAPSHOT"] ; not ready for use
-                 [criterium "0.4.4"]
+                 [criterium "0.4.4"] ; to use, e.g.: (use '[criterium.core :as c])
                  ])
