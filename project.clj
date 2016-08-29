620b4677acd82811f5225813e4183316533ec9e6
mars0i <mabrams@uab.edu>
2016-08-29 12:43:25 -0500

    added commented out Clojure 1.9.0 alpha

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Gnu General Public License version 3.0"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;[org.clojure/clojure "1.9.0-alpha11"]
                  [net.mikera/core.matrix "0.54.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [thinktopic/aljabr "0.1.1"] ; ignore [... "0.4.0-SNAPSHOT"] in README.md--wishful thinking
