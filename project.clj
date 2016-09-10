cb9c2b362786cafae17dd72a12a6ccac908881c3
mars0i <mabrams@uab.edu>
2016-09-10 15:24:57 -0500

    added numeric-tower

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Gnu General Public License version 3.0"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;[org.clojure/clojure "1.9.0-alpha11"]
+                 ;[org.clojure/clojure "1.9.0-alpha12"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [net.mikera/core.matrix "0.54.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [thinktopic/aljabr "0.1.1"] ; ignore [... "0.4.0-SNAPSHOT"] in README.md--wishful thinking
                  [clatrix "0.5.0"]
                  ;[org.clojars.ds923y/nd4clj "0.1.0-SNAPSHOT"] ; not ready for use
-                 [incanter "1.5.7"]
                  [criterium "0.4.4"] ; to use, e.g.: (use '[criterium.core :as c])
-                 ])
+                 [incanter "1.5.7"]])
