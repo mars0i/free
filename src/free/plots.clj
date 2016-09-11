3e9a02a30de4f60d82f795064228f865cefe148f
mars0i <mabrams@uab.edu>
2016-09-11 12:54:55 -0500

    switched phi to scatter-plot, other tweaks

--- src/free/plots.clj
+++ src/free/plots.clj
@@ -8,13 +8,16 @@
   [level-num stages]
   (let [level-stages (map #(nth % level-num) stages)]
     (doto 
-      (xy-plot   (range) (map :phi   level-stages) :series-label "phi" :legend true)
+      ;; Need scatter plot for phi for the sake of level 0, where inputs are noisy
+      (scatter-plot (range) (map :phi level-stages) :series-label "phi" :legend true)
+      ;; For other vars, better to use lines (as in xy-plot):
       (add-lines (range) (map :eps   level-stages) :series-label "eps")
       (add-lines (range) (map :sigma level-stages) :series-label "sigma")
       (add-lines (range) (map :theta level-stages) :series-label "theta")
-      (set-stroke-color java.awt.Color/green :dataset 0) ; phi. For xy-plot and add-lines use :dataset instead of :series.
-      (set-stroke-color java.awt.Color/black :dataset 1) ; eps. This is a bug in Incanter. See issue #233 in Incanter repo.
-      (set-stroke-color java.awt.Color/red   :dataset 2) ; sigma
+      (set-point-size 1) ; applies to points, not lines. apparently only applies to the first series.
+      (set-stroke-color java.awt.Color/black :dataset 0) ; phi. For xy-plot and add-lines use :dataset instead of :series.This is a bug in Incanter. See issue #233 in Incanter repo.
+      (set-stroke-color java.awt.Color/red   :dataset 1) ; eps. 
+      (set-stroke-color java.awt.Color/green :dataset 2) ; sigma
       (set-stroke-color java.awt.Color/blue  :dataset 3) ; theta
       (view))))
 
