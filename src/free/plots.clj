8702667efa50c037aee6a735b2b81d0bc2026130
mars0i <mabrams@uab.edu>
2016-09-19 01:03:05 -0500

    added version that accepts stages with a level already extracted

--- src/free/plots.clj
+++ src/free/plots.clj
@@ -5,25 +5,27 @@
   "Plot phi, eps, sigma, and theta for all level records at level level-num
   in sequence stages.  If n is provided, take only n elements from stages.
   If every is also provided, use only the elements that are every steps apart.
-  Level 0 is the first level.  NOTE: Assumes all values are scalars.  Returns 
-  the plot object."
+  Level 0 is the first level.  If level-num is not provided, assumes that stages
+  is a sequence consisting only of entities at that level.  
+  NOTE: Assumes all values are scalars.  Returns the plot object."
   ([stages level-num n every]
    (plot-level (take-nth every (take n stages)) level-num))
   ([stages level-num n]
    (plot-level (take n stages) level-num))
   ([stages level-num]
-   (let [level-stages (map #(nth % level-num) stages)]
+   (plot-level (map #(nth % level-num) stages)))
+  ([stages]
      (doto 
        ;; Need scatter plot for phi for the sake of level 0, where inputs are noisy
-       (scatter-plot (range) (map :phi level-stages) :series-label "phi" :legend true)
+       (scatter-plot (range) (map :phi stages) :series-label "phi" :legend true)
        ;; For other vars, better to use lines (as in xy-plot):
-       (add-lines (range) (map :eps   level-stages) :series-label "eps")
-       (add-lines (range) (map :sigma level-stages) :series-label "sigma")
-       (add-lines (range) (map :theta level-stages) :series-label "theta")
+       (add-lines (range) (map :eps   stages) :series-label "eps")
+       (add-lines (range) (map :sigma stages) :series-label "sigma")
+       (add-lines (range) (map :theta stages) :series-label "theta")
        (set-point-size 1) ; applies to points, not lines. apparently only applies to the first series.
        (set-stroke-color java.awt.Color/black :dataset 0) ; phi. For xy-plot and add-lines use :dataset instead of :series.This is a bug in Incanter. See issue #233 in Incanter repo.
        (set-stroke-color java.awt.Color/red   :dataset 1) ; eps. 
        (set-stroke-color java.awt.Color/green :dataset 2) ; sigma
        (set-stroke-color java.awt.Color/blue  :dataset 3) ; theta
-       (view :width 800 :height 600)))))
+       (view :width 800 :height 600))))
 
