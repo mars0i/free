1ced9000ca21784c0492c56041459d284a68825d
mars0i <mabrams@uab.edu>
2016-09-29 00:14:56 -0500

    fixed def of rand-idx

--- src/utils/random.cljs
+++ src/utils/random.cljs
@@ -20,9 +20,14 @@
      (flush-rng rng)
      rng))) 
 
-(defn rand-idx [rng n] (.integer rng n))
+(defn rand-idx
+  [rng n]
+  (.integer rng
+            (clj-js {:min 0 :max (dec n)}))) ; min and max are allowable values
 
-(defn next-long [rng] (.integer rng))
+(defn next-long
+  [rng]
+  (.integer rng))
 
 (defn next-double
 "Returns a random double in the half-open range from [0.0,1.0)."
