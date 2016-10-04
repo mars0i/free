eb683bd0a2b03d39bd2d66a5a264905e0151d84d
mars0i <mabrams@uab.edu>
2016-10-03 22:49:01 -0500

    added placeholder plots.cljs files

---
+++ src/cljs/matrix/free/plots.cljs
@@ -0,0 +1,4 @@
+(ns free.plots)
+
+(enable-console-print!)
+(println "src/cljs/matrix/free/plots.cljs doesn't do anything yet.")
---
+++ src/cljs/scalar/free/plots.cljs
@@ -0,0 +1,4 @@
+(ns free.plots)
+
+(enable-console-print!)
+(println "src/cljs/scalar/free/plots.cljs doesn't do anything yet.")
