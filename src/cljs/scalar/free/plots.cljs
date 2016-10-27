fdee18142aa6cf1b62e9c38d3cf62ca670a012c2
mars0i <mabrams@uab.edu>
2016-10-26 23:13:01 -0500

    reorg so only model.cljs not experimental models in cljs path
    that way only model.cljs gets compiled into cljs js file.
    might move some back later.

--- src/cljs/scalar/free/plots.cljs
+++
@@ -1,8 +0,0 @@
-;; This software is copyright 2016 by Marshall Abrams, and is distributed
-;; under the Gnu General Public License version 3.0 as specified in the
-;; the file LICENSE.
-
-(ns free.plots)
-
-(enable-console-print!)
-(println "src/cljs/scalar/free/plots.cljs doesn't do anything yet.")
