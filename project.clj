e86dd68f3393508c43345ce87ee391af992941f0
mabrams <mabrams@uab.edu>
2016-11-27 23:49:06 -0600

    rearranged some lines for clarity

--- project.clj
+++ project.clj
@@ -56,20 +56,20 @@
                                          "src/cljc/general" "src/cljc/matrix"]
                           :java-source-paths ["src/java"]}
 
-             :cljs-scalar {:source-paths ["src/cljs/general"  "src/cljs/scalar"
+             :cljs-scalar {:source-paths ["src/cljs/general" "src/cljs/scalar"
                                           "src/cljc/general" "src/cljc/scalar"
-                                          "src/clj/general"  "src/clj/scalar"   ; kludge to make Clojure compilation stage happier?
-                                          "src/cljc/general" "src/cljc/scalar"] ; kludge to make Clojure compilation stage happier?
-                          :java-source-paths ["src/java"]}                      ; kludge to make Clojure compilation stage happier?
+                                          "src/clj/general"  "src/clj/scalar"] ; kludge to make Clojure compilation stage happier?
+                          :java-source-paths ["src/java"]}                     ; kludge to make Clojure compilation stage happier?
 
              :cljs-matrix {:source-paths ["src/cljs/general"  ; "src/cljs/matrix" ; currently empty/nonexistent
-                                          "src/cljc/general" "src/cljc/matrix"]}
+                                          "src/cljc/general" "src/cljc/matrix"]
+                          :java-source-paths ["src/java"]}                     ; kludge to make Clojure compilation stage happier?
             }
 
   :cljsbuild {:builds
               [{:id "dev-scalar"
-                :source-paths ["src/cljc/general" "src/cljc/scalar"
-                               "src/cljs/general" "src/cljs/scalar"
+                :source-paths ["src/cljs/general" "src/cljs/scalar"
+			       "src/cljc/general" "src/cljc/scalar"
                                "dev"]
                 ;; The presence of a :figwheel configuration here will cause figwheel to inject the figwheel client into your build
                 :figwheel {:on-jsload "free.core/on-js-reload"
@@ -87,13 +87,12 @@
                            :pretty-print false
                            :optimizations :none
                            :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
+                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome https://github.com/binaryage/cljs-devtools
                            :preloads [devtools.preload]
                            :external-config {:devtools-config {:features-to-install [:formatters :hints]}}}}
                {:id "dev-matrix"
-                :source-paths ["src/cljc/general" "src/cljc/matrix"
-                               "src/cljs/general" ; "src/cljs/matrix" ; currently empty/nonexistent
+                :source-paths ["src/cljs/general" ; "src/cljs/matrix" ; currently empty/nonexistent
+			       "src/cljc/general" "src/cljc/matrix"
                                "dev"]
                 ;; The presence of a :figwheel configuration here will cause figwheel to inject the figwheel client into your build
                 :figwheel {:on-jsload "free.core/on-js-reload"
@@ -111,8 +110,7 @@
                            :pretty-print false
                            :optimizations :none
                            :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
+                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome https://github.com/binaryage/cljs-devtools
                            :preloads [devtools.preload]
                            :external-config {:devtools-config {:features-to-install [:formatters :hints]}}}}
                ;; This next build is an compressed minified build for production. You can build this with: lein cljsbuild once min
